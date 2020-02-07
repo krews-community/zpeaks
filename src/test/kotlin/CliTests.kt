7954a6c2b34558576b947d000e0c1ff1156c7f59
hpratt <hpratt111@live.com>
2020-02-07 18:43:35 -0500

    fix test

--- src/test/kotlin/CliTests.kt
+++ src/test/kotlin/CliTests.kt
@@ -28,19 +28,36 @@ class CliTests {
             """.trimIndent().split("\\s+".toRegex())
         ZPeaksCommand(run = mockRun).main(args)
 
-        val expectedRunConfig = ZRunConfig(
-            pileUpRunner = BamPileUpRunner(listOf(
-                PileUpInput(MULTI_BAM_1_PATH, PileUpOptions(Strand.BOTH, PileUpAlgorithm.START, 5, 10)),
-                PileUpInput(MULTI_BAM_2_PATH, PileUpOptions(Strand.PLUS, PileUpAlgorithm.LENGTH, -5, 15))
-            )),
-            chrFilter = mapOf("chr22" to (30000000 until 30500000)),
-            signalOut = SignalOutput(signalOut, SignalOutputType.RAW, SignalOutputFormat.WIG, signalResolution = 2),
-            peaksOut = peaksOut,
-            smoothing = 60.0,
-            threshold = 7.0,
-            parallelism = 4
-        )
-        verify { mockRun(any(), expectedRunConfig, false) }
+        fun matchPileUpInput(it: PileUpInput, other: PileUpInput): Boolean {	
+	    return it.bam.equals(other.bam) && it.options.strand.equals(other.options.strand) &&
+                it.options.pileUpAlgorithm.equals(other.options.pileUpAlgorithm) &&
+		it.options.forwardShift == other.options.forwardShift &&
+		it.options.reverseShift == other.options.reverseShift
+        }
+	
+        fun matchBamPileUpRunner(it: BamPileUpRunner, other: BamPileUpRunner): Boolean {
+	    if (it.pileUpInputs.size != other.pileUpInputs.size) return false
+	    var matched = true	
+	    other.pileUpInputs.forEachIndexed { i, v ->
+	        if (!matchPileUpInput(it.pileUpInputs[i], v)) matched = false
+	    }
+	    return matched
+        }
+
+        verify { mockRun(any(), match {
+	    it.chrFilter != null && it.chrFilter!!.equals(mapOf("chr22" to (30000000 until 30500000))) &&
+	    it.peaksOut != null && it.peaksOut!!.equals(peaksOut) &&
+	    it.smoothing == 60.0 && it.threshold == 7.0 && it.parallelism == 4 &&
+  	    it.signalOut != null && it.signalOut!!.equals(
+	        SignalOutput(signalOut, SignalOutputType.RAW, SignalOutputFormat.WIG, signalResolution = 2)
+	    ) &&
+	    it.pileUpRunner is BamPileUpRunner && matchBamPileUpRunner(
+		it.pileUpRunner as BamPileUpRunner, BamPileUpRunner(listOf(
+                    PileUpInput(MULTI_BAM_1_PATH, PileUpOptions(Strand.BOTH, PileUpAlgorithm.START, 5, 10)),
+                    PileUpInput(MULTI_BAM_2_PATH, PileUpOptions(Strand.PLUS, PileUpAlgorithm.LENGTH, -5, 15))
+                ))
+	    )
+	}, false) }
     }
 
 }
